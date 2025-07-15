4272737bf4857c3887bc24be4d373d16b12f366c
Tejashvi Gautam <tejashvigautam@gmail.com>
2025-07-15 13:09:29 +0530

    Allow CORS from deployed frontend

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // frontend URL
+    origin: ["http://localhost:5173", "https://yappie-eta.vercel.app"],
+
     methods: ["GET", "POST"]
   }
 });
